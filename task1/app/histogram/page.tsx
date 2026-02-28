265f2b992733cf2247700df16b4a0b427f0c831d
Abdelrahman Khalaf <abdlrhmankhalaf566@gmail.com>
2026-03-01 01:15:00 +0200

    inital commit in client

--- task1/app/histogram/page.tsx
+++
@@ -1,9 +0,0 @@
-import { HistogramTab } from "@/components/HistogramTab";
-
-export default function HistogramPage() {
-  return (
-    <main className="flex-1 p-6 overflow-auto">
-      <HistogramTab />
-    </main>
-  );
-}
