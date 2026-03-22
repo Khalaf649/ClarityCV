25a6b9adb563ff6e6622128c8274ac4427e5a523
Menna1812 <14712022101664@stud.cu.edu.eg>
2026-03-22 21:04:51 +0200

    solving some issues

--- backend/include/processing/HoughProcessor.hpp
+++ backend/include/processing/HoughProcessor.hpp
@@ -17,18 +17,18 @@ struct HoughParams {
 
 struct HoughResult {
     cv::Mat transformImage;
-    // std::vector<cv::Vec4i> lines;
-    // std::vector<cv::Vec3f> circles;
-    // std::vector<cv::Vec4f> ellipses;
+    std::vector<cv::Vec4i> lines;
+    std::vector<cv::Vec3f> circles;
+    std::vector<cv::Vec4f> ellipses;
 };
 
 class HoughProcessor {
 public:
     static HoughResult apply(const cv::Mat& input, const HoughParams& params);
-// private:
-//     static HoughResult applyLine(const cv::Mat& input, const HoughParams& params);
-//     static HoughResult applyCircle(const cv::Mat& input, const HoughParams& params);
-//     static HoughResult applyEllipse(const cv::Mat& input, const HoughParams& params);
+private:
+    static HoughResult applyLine(const cv::Mat& input, const HoughParams& params);
+    static HoughResult applyCircle(const cv::Mat& input, const HoughParams& params);
+    static HoughResult applyEllipse(const cv::Mat& input, const HoughParams& params);
 };
 
 } // namespace processing
